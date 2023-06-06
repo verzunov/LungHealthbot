32921650ea02246a2d6138887916b0267ac9331c
Verzunov Sergei <verzunov@hotmail.com>
2023-06-06 18:19:32 +0600

    Версия 0.1

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ if __version_info__ < (20, 0, 0, "alpha", 1):
         f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
     )
 from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters, CallbackQueryHandler
 
 import os
 from dotenv import load_dotenv
@@ -41,11 +41,17 @@ import numpy as np
 from keras.preprocessing import image
 from keras import utils
 from tensorflow import keras
+from keras.preprocessing.image import ImageDataGenerator
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from PIL import Image
+import requests
+from io import BytesIO
+
 load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
 
-model = keras.models.load_model('lung')
+model = keras.models.load_model('KerasModel')
 print(dir(model))
 # Enable logging
 logging.basicConfig(
@@ -53,6 +59,8 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+path = './images/'
+test_datagen = ImageDataGenerator(rescale=1./255)
 
 # Define a few command handlers. These usually take the two arguments update and
 # context.
@@ -60,59 +68,75 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
     await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True),
+"""
+Добро пожаловать в телеграмм-бот для анализа рентгеновских снимков для диагностики заболеваний легких!
+Мы рады приветствовать вас в этом инновационном сервисе, который поможет вам быстро и точно оценить состояние ваших легких на основе рентгеновских снимков. Наш бот основан на передовых алгоритмах и искусственном интеллекте, чтобы предоставить вам качественные результаты.
+Загрузите ваш рентгеновский снимок, и наш бот проведет анализ, выявив возможные патологии и помогая вам лучше понять ваше состояние. Наша система обучена распознавать различные заболевания, включая пневмонию, туберкулез, Ковид-19 и отличать их от снимков здоровых легких. Получите быстрый и надежный диагноз прямо здесь!
+Пожалуйста, имейте в виду, что результаты, предоставляемые нашим ботом, не заменяют медицинскую консультацию специалиста. Они являются лишь предварительной оценкой и рекомендуется обратиться к врачу для окончательного диагноза.
+Мы надеемся, что наш телеграмм-бот сможет облегчить процесс диагностики и помочь вам заботиться о вашем здоровье. Не стесняйтесь задавать вопросы, если у вас возникнут, и давайте начнем!
+
+Загрузите изображение с камеры или из галереи.
+Также вы можете отправить ссылку на изображение. Примеры ссылок:
+<a href="https://www.kenhub.com/thumbor/AkXFsw0396y894sLEMWlcDuChJA=/fit-in/800x1600/filters:watermark(/images/logo_url.png,-10,-10,0):background_color(FFFFFF):format(jpeg)/images/library/10851/eXtmE1V2XgsjZK2JolVQ5g_Border_of_left_atrium.png"> Норма </a>
+<a href="https://www.wikidoc.org/images/8/85/Pulmonary_Tuberculosis_X-ray3.jpg?20140905151403"> Туберкулез </a>
+<a href="https://storage.googleapis.com/kagglesdsdata/datasets/1592399/2619910/test/PNEUMONIA/person100_bacteria_475.jpeg?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=databundle-worker-v2%40kaggle-161607.iam.gserviceaccount.com%2F20230606%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20230606T111645Z&X-Goog-Expires=345600&X-Goog-SignedHeaders=host&X-Goog-Signature=0b063c5274f1d54d73533a65e97a09906784684ebae1f34cf86cb00467ade420093a39e50da191ef1f973501425601657892ba10f0748ef50ccdd2c49910724735043d6ed6083a2a63438c1593ce4bcdaa97b333d203034e7f0c052360c475de0ebe1d7d287eae7d8b947e34130fb613651924eda406fac52a38f444fe4e3641f12bd1de25174ef93da0c03ebb97dfb367c8acf09b9446daace458457d7398145fb2a523471de8fc8cb38f0fc1b552e74db1f3d95e4e1cf20a2dad0ed91d492bfc1dca609cca2b586d21a819e25c60a726f0c2a9a7cb25f8f8f57c05f11dc2da19fd9145345fb31f7f29d9cf289dd1c99e3dc53bf09c7308765e938c838ee276"> Пневмония </a>
+<a href="https://tidsskriftet.no/sites/default/files/styles/default_scale/public/article--2020--04--20-0332--KRO_20-0332-01_ENG.jpg?itok=RnC4A_09"> Ковид-19 </a>
+"""
     )
-
-async def version_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(keras.__version__)
-
-def load_image(img, H=320, W=320):
-    """Load and preprocess image."""
-    img_path = img
-    #mean, std = get_mean_std_per_batch(img_path, df, H=H, W=W)
-    x = utils.load_img(img_path, target_size=(H, W))
-
-    return np.array(x).reshape((1 ,H, W, 3))
-
 
 async def upload_image(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     message = update.message
+    try:
+        if message.text:
+            response = requests.get(message.text)
+            img = Image.open(BytesIO(response.content))
+            img.save("./images/test/test.jpg")
+
+    except:
+        await update.message.reply_text("Изображение из этого источника не может быть загружено.")
+
+        return
     if message.photo:
         # Телеграмм отправляет фото в различных размерах, мы возьмем самое большое
         photo = message.photo[-1]
         file = await context.bot.getFile(photo.file_id)
-        await file.download_to_drive("test.jpg")
-        f=load_image("test.jpg")
-        print(f.shape)
-        res=model.predict(f)
-        p=res[0][0]
-        print(res)
-        await update.message.reply_text(f"Вероятность туберкулеза - {p}")
-    
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /help is issued."""
-    await update.message.reply_text("Help!")
+        await file.download_to_drive("./images/test/test.jpg")
+    test_generator = test_datagen.flow_from_directory(directory=path,
+                                                  target_size=(224, 224),
+                                                  color_mode="grayscale",
+                                                  batch_size=1,
+                                                  class_mode=None,
+                                                  shuffle=False,
+                                                  seed=42)
+    #Predict Output
+    STEP_SIZE_TEST = test_generator.n//test_generator.batch_size
+    test_generator.reset()
+    pred=model.predict(test_generator,
+    steps=STEP_SIZE_TEST,
+    verbose=1)
 
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Echo the user message."""
-    await update.message.reply_text(update.message.text)
-
+    predicted_class_indices=np.argmax(pred,axis=1)
+    labels = {0: 'Ковид19', 1: 'Норма', 2: 'Пневмония', 3: 'Туберкулез'}
+    predictions = [labels[k] for k in predicted_class_indices]
+    res=dict(zip(list(labels.values()),list(pred[0])))
+    sorted_tuple=sorted(res.items(),key=lambda x:x[1],reverse=True)
+    converted_res = dict(sorted_tuple)
+    ans="Вероятность:\n"
+    for k,v in converted_res.items():
+        per=v*100
+        ans+= f"{k} -- {per:.1f}%\n"
+    print(converted_res)
+    await update.message.reply_text(ans)
 
 def main() -> None:
     """Start the bot."""
     # Create the Application and pass it your bot's token.
-    application = Application.builder().token("6283015242:AAF1Emzw2X2GY0kbT5FIu-bDHgk0Xwr5oOQ").build()
+    application = Application.builder().token(TOKEN).build()
 
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help_command))
-    application.add_handler(CommandHandler("version", version_command))
     application.add_handler(MessageHandler(filters.ALL, upload_image))
 
-    # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling()
